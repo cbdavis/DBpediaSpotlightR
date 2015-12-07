43775a02edfc5ca9ec8dabe87cb4a36f90f756de
Chris Davis <c_b_dvs@yahoo.com>
2015-12-07 13:39:46 +0100

    improving documentation

--- R/DBpediaSpotlightR.R
+++ R/DBpediaSpotlightR.R
@@ -7,6 +7,19 @@
 library(httr)
 library(jsonlite)
 
+#' @name dbpediaSpotlight
+#' @description Download annotations from the DBpedia Spotlight service
+#'
+#' @param text The text to be processed.  Character vectors are currently not supported
+#' @param apiURL The URL of the API service.  By default, the official DBpedia Spotlight API will be used.  If \code{"sztaki"} is set, then the one hosted by sztaki.hu will be used.  A custom URL can be specified as well.
+#' @param saveResponse Whether the response from the API will be saved or not
+#' @param responseFileName If \code{saveResponse=TRUE}, then this will be the file where the json response is saved.  This file will contain the results of the analysis along with a record of the original text to be parsed.
+#' @param verbose Whether curl should be verbose when sending and retrieving the data from the API
+#' @return A data frame containing information related to the entities extracted from the text.
+#' @examples
+#' dbpediaSpotlight("Berlin was the capital of the Kingdom of Prussia")
+#' dbpediaSpotlight("Berlin was the capital of the Kingdom of Prussia", apiURL="sztaki")
+#' dbpediaSpotlight("Berlin was the capital of the Kingdom of Prussia", apiURL="sztaki", saveResponse=TRUE, responseFileName="Berlin.json")
 dbpediaSpotlight <- function(text, apiURL = "dbpedia", saveResponse=FALSE, responseFileName="", verbose=TRUE) {
   if (apiURL == "dbpedia"){
     annotationURL = "http://spotlight.dbpedia.org/rest/annotate"
