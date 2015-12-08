7d9502e5119580194cf8394ed0c9afe0b57d364c
Chris Davis <c_b_dvs@yahoo.com>
2015-12-08 15:31:03 +0100

    simplifying function API

--- R/DBpediaSpotlightR.R
+++ R/DBpediaSpotlightR.R
@@ -12,8 +12,7 @@ library(jsonlite)
 #'
 #' @param text The text to be processed.  Character vectors are currently not supported
 #' @param apiURL The URL of the API service.  By default, the official DBpedia Spotlight API will be used.  If \code{"sztaki"} is set, then the one hosted by sztaki.hu will be used.  A custom URL can be specified as well.
-#' @param saveResponse Whether the response from the API will be saved or not
-#' @param responseFileName If \code{saveResponse=TRUE}, then this will be the file where the json response is saved.  This file will contain the results of the analysis along with a record of the original text to be parsed.
+#' @param responseFileName If specified, this will be the file where the json response is saved, otherwise only the data frame will be returned by this function.  This file will contain the results of the analysis along with a record of the original text to be parsed.
 #' @param verbose Whether curl should be verbose when sending and retrieving the data from the API
 #' @return A data frame containing information related to the entities extracted from the text.
 #' @examples
@@ -22,10 +21,9 @@ library(jsonlite)
 #' dbpediaSpotlight("Berlin was the capital of the Kingdom of Prussia", apiURL="sztaki")
 #' dbpediaSpotlight("Berlin was the capital of the Kingdom of Prussia",
 #'                  apiURL="sztaki",
-#'                  saveResponse=TRUE,
 #'                  responseFileName="Berlin.json")
 #' @export
-dbpediaSpotlight <- function(text, apiURL = "dbpedia", saveResponse=FALSE, responseFileName="", verbose=TRUE) {
+dbpediaSpotlight <- function(text, apiURL = "dbpedia", responseFileName="", verbose=TRUE) {
   if (apiURL == "dbpedia"){
     annotationURL = "http://spotlight.dbpedia.org/rest/annotate"
   } else if (apiURL == "sztaki"){
@@ -42,14 +40,10 @@ dbpediaSpotlight <- function(text, apiURL = "dbpedia", saveResponse=FALSE, respo
 
   jsonText = rawToChar(response$content)
 
-  if (saveResponse){
-    if (responseFileName != ""){
-      fileConn<-file(responseFileName)
-      writeLines(jsonText, fileConn)
-      close(fileConn)
-    } else {
-      warning("responseFileName needs to be specified if saveResponse=TRUE")
-    }
+  if (responseFileName != ""){
+    fileConn<-file(responseFileName)
+    writeLines(jsonText, fileConn)
+    close(fileConn)
   }
 
   jsonData = fromJSON(jsonText)
